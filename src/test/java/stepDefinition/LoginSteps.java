014445c5770a2e3368b7e2759226e596dcb0d107
Mujahid92-me <mujahidcomedian@gmail.com>
2025-02-13 12:15:27 +0530

    New changes

---
+++ src/test/java/stepDefinition/LoginSteps.java
@@ -0,0 +1,71 @@
+package stepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+	@Given("user navigates to facebook website")
+	public void user_navigates_to_facebook_website() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("user validates the home page title")
+	public void user_validates_the_home_page_title() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("user enters username")
+	public void user_enters_username() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("user enters password")
+	public void user_enters_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("user clicks on signin button")
+	public void user_clicks_on_signin_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("I want to write a step with name1")
+	public void i_want_to_write_a_step_with_name1() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("I check for the {int} in step")
+	public void i_check_for_the_in_step(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("I verify the success in step")
+	public void i_verify_the_success_in_step() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("I want to write a step with name2")
+	public void i_want_to_write_a_step_with_name2() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("I verify the Fail in step")
+	public void i_verify_the_fail_in_step() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+
+
+}
